a01e7252125098c435d89bac209fb727205eee68
jsos-py <oportojose662@gmail.com>
2025-04-21 13:30:19 -0400

    avances de la clase arreglos

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,6 @@
-#hola que pa fhjkfhs
+#hola que pa fhjkfhs 
+estudiantes = [ 
+    { "nombre" : "maxi" , "edad" : 20, "curso" : "2a"}
+    { "nombre" : "richart" , "edad" : 19 , "curso" : "1b"}
+]
+print(estudiantes[0][´nombre´])
